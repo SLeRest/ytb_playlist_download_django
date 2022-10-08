3fed59773884286efcfead5c1b40038ce374662e
SLeRest <sebastienlerest22@gmail.com>
2022-10-08 13:09:57 +0200

    ajout de entrypoint, seyup du dockerfile de l'app, ajout des package dans requirements, debut de model dans les grandes ligne

--- app/models.py
+++ app/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+
+class Album(models.Model):
+    name = models.CharField(max_length=255)
+    artist = models.CharField(max_length=255)
+    album = models.CharField(max_length=255)
+    release_date = models.DateField()
+    downloaded = models.DateTimeField(default=None) # TODO a verif pour le default
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+class Song(models.Model):
+    name = models.CharField(max_length=255)
+    artist = models.CharField(max_length=255)
+    # TODO album associe
+    release_date = models.DateField()
+    downloaded = models.DateTimeField(default=None) # TODO a verif pour le default
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
