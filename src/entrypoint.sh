f6eae08a8e7ee167941893755a1823bec58b29e8
SLeRest <sebastienlerest22@gmail.com>
2022-10-18 00:03:07 +0200

    change models

--- src/entrypoint.sh
+++ src/entrypoint.sh
@@ -5,4 +5,5 @@ python3 manage.py createsuperuser \
     --noinput \
     --username $DJANGO_SUPERUSER_USERNAME \
     --email $DJANGO_SUPERUSER_EMAIL
+#pyhton3 manage.py shell << playlist/fixture.py
 python3 manage.py runserver 0.0.0.0:80
--- src/playlist/models.py
+++ src/playlist/models.py
@@ -1,30 +1,37 @@
 from django.db import models
 
-class Base(models.Model):
+class Song(models.Model):
+    name = models.CharField(max_length=255, null=True, default=None)
+    artist = models.CharField(max_length=255, null=True, default=None)
+    album = models.CharField(max_length=255, null=True, default=None)
+    release_date = models.DateField(null=True, default=None)
+    downloaded = models.DateTimeField(null=True, default=None)
+    song_data = models.BinaryField(null=True, default=None)
+    format = models.CharField(null=True, max_length=20) # TODO a list of choice and it can't be mull
+    youtube_url = models.CharField(max_length=255) # TODO check if valid url
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
 
+    def __str__(self):
+        return self.name
     class Meta:
         ordering = ('created', )
+        db_table = 'song'
 
-class Song(Base):
+class Playlist(models.Model):
     name = models.CharField(max_length=255)
-    artist = models.CharField(max_length=255)
-    album = models.CharField(max_length=255)
-    release_date = models.DateField(null=True, default=None)
-    downloaded = models.DateTimeField(default=None) # TODO a verif pour le default
-    song_data = models.BinaryField(default=None)
-    format = models.CharField(max_length=20) # TODO a list of choice
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.name
-
-class Playlist(Base):
-    name = models.CharField(max_length=255)
-
-    def __str__(self):
-        return self.name
+    class Meta:
+        ordering = ('created', )
+        db_table = 'playlist'
 
 class PlaylistSongs(models.Model):
     playlist = models.ForeignKey(Playlist, on_delete=models.CASCADE)
     song = models.ForeignKey(Song, on_delete=models.CASCADE)
+
+    class Meta:
+        db_table = 'playlist_songs'
